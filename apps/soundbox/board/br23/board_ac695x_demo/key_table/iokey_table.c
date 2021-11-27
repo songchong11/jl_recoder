2b0e35b05889035093e67b3805ca48c84dbb5bc3
songchong <1972433980@qq.com>
2021-11-27 15:28:45 +0800

    修复pc模式下按按键上报键值的问题

--- apps/soundbox/board/br23/board_ac695x_demo/key_table/iokey_table.c
+++ apps/soundbox/board/br23/board_ac695x_demo/key_table/iokey_table.c
@@ -120,16 +120,16 @@ const u16 music_key_io_table[KEY_IO_NUM_MAX][KEY_EVENT_MAX] = {
 const u16 pc_key_io_table[KEY_IO_NUM_MAX][KEY_EVENT_MAX] = {
     //单击             //长按          //hold         //抬起            //双击                //三击
     [0] = {
-        KEY_MUSIC_PP,			KEY_POWEROFF,			KEY_POWEROFF_HOLD,	KEY_NULL,	KEY_NULL,			KEY_NULL
+        KEY_NULL,			KEY_POWEROFF,			KEY_POWEROFF_HOLD,	KEY_NULL,	KEY_NULL,			KEY_NULL
     },
     [1] = {
-        KEY_VOL_UP,				KEY_VOL_UP,				KEY_VOL_UP,			KEY_NULL,	KEY_NULL,			KEY_NULL
+        KEY_NULL,				KEY_NULL,				KEY_NULL,			KEY_NULL,	KEY_NULL,			KEY_NULL
     },
     [2] = {
-        KEY_VOL_DOWN,			KEY_VOL_DOWN,			KEY_VOL_DOWN,		KEY_NULL,	KEY_NULL,			KEY_NULL
+        KEY_NULL,			KEY_NULL,			KEY_NULL,		KEY_NULL,	KEY_NULL,			KEY_NULL
     },
     [3] = {
-        KEY_CHANGE_MODE,		KEY_NULL,				KEY_NULL,			KEY_NULL,	KEY_NULL,			KEY_NULL
+        KEY_NULL,		KEY_NULL,				KEY_NULL,			KEY_NULL,	KEY_NULL,			KEY_NULL
     },
     [4] = {
         KEY_NULL,				KEY_NULL,				KEY_NULL,			KEY_NULL,	KEY_NULL,			KEY_NULL
